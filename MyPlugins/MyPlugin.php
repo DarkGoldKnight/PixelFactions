8e276130bb2d9dc49cbe61f4c3708057aaa74841
DarkGoldKnight <30422975+DarkGoldKnight@users.noreply.github.com>
2017-07-25 13:29:32 +1200

    Update MyPlugin.php

--- MyPlugins/MyPlugin.php
+++ MyPlugins/MyPlugin.php
@@ -1,1 +1,13 @@
-
+<?php
+namespace yourname\myplugin;
+use pocketmine\plugin\PluginBase;
+class MyPlugin extends PluginBase
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    $commandName = $command->getName();
+    if($commandName === "greet"){
+      $sender->sendMessage("Hi!");
+      return true;
+    }
+    return false;
+  }{
+}
